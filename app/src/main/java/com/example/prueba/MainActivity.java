1daf1d778f16bcdd9a910c32894100c98ea9e5dc
Ricardo Martinez <ricardojosea74@gmail.com>
2019-08-13 14:18:23 -0600

    validaciones

--- app/src/main/java/com/example/prueba/MainActivity.java
+++ app/src/main/java/com/example/prueba/MainActivity.java
@@ -68,10 +68,7 @@ public class MainActivity extends AppCompatActivity
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+
 
         return super.onOptionsItemSelected(item);
     }
@@ -97,10 +94,6 @@ public class MainActivity extends AppCompatActivity
             transaction.replace(R.id.content_main,new GreenFragment()).commit();
 
         } else if (id == R.id.nav_tools) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
 
         }
 
